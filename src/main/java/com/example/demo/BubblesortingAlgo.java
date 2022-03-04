88051042d1ff48a7f34e1340b5ad51034cb92432
honey <honey.arora@tothenew.com>
2022-03-04 11:01:59 +0530

    committing binarysearchimp

---
+++ src/main/java/com/example/demo/BubblesortingAlgo.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component     //bean
+public class BubblesortingAlgo implements  SortingAlgorithm{
+   public int[] sort(int[] number){
+       return number;
+   }
+}
